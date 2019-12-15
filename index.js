2660666cb9f3e9c54c985ec9d64512d5b95cff02
nozdev <nozdev@gmail.com>
2019-12-15 03:05:56 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
+const { Pool } = require('pg')
 const PORT = process.env.PORT || 5000
-const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
