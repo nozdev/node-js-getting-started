eed79655a940f887235836429906cdd45280df72
nozdev <nozdev@gmail.com>
2019-12-15 03:18:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,19 +6,22 @@ const pool = new Pool({
   connectionString: "postgres://noykjcqbgyuibe:f252676805c48bbc12d4b3ac82b7cfcc59a7c0f230e16dc6592bd62bc61212a1@ec2-174-129-255-7.compute-1.amazonaws.com:5432/ddq64mf9n2iogo",
   ssl: true
 });
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index')
-      .get('/db', async (req, res) => {
+  .get('/', (req, res) => res.render('pages/index'))
+.get('/db', async (req, res) => {
     try {
-
-      res.render('pages/db', null );
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
       client.release();
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
     }
-  }))
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
